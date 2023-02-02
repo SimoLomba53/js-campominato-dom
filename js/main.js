dec69101f9a336d5ac831d0b48a40a20fea5d89b
SimoLomba53 <simolomba253@gmail.com>
2023-02-02 14:55:02 +0100

    problemiinserire array number nelle celle

--- js/main.js
+++ js/main.js
@@ -33,12 +33,16 @@ generateButton.addEventListener(
                     this.classList.toggle("clickcolor");
                 }
             )
+          
         }
+      
         return;
     }
-    
+
      
 )
+
+
 //PC GENERA 16 NUMERI NEL RANGE DELLA DIFFICOLTA SCELTA(nell'array non possono esserci numeri uguali)
 let bomb=[];
 
@@ -46,9 +50,13 @@ for (let i=0;i<16;i++){
     const randomNumber=parseInt(Math.floor(Math.random()*100));
     console.log(randomNumber[i]);
     bomb.push(randomNumber);
+    square.append(bomb[i]);
+    
 }
 
 //INSERIRE QUESTI NUMERI ALL'INTERNO DELLE CELLE
+
+
 //FAR SI CHE SE VIENE CLICCATA UNA CELLA CON UNO DI QUESTI NUMERI SI COLORA DI ROSSO E LA PARTITA TERMINA
 //ALTRIMENTI DI AZZURRO E LA PARTITA CONTINUA
 //COMUNICARE QUANTE CELLE SONO STATE CLICCATE PRIMA DEL TERMINE DELLA PARTITA
